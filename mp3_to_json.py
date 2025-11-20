5de6fed46228e67390849d1b8d5990633f14d491
Bupendra <bupendra.d123@gmail.com>
2025-11-20 13:12:06 +0530

    Fresh clean commit - RAG AI Teaching Assistant

---
+++ mp3_to_json.py
@@ -0,0 +1,44 @@
+import whisper
+import json
+import os
+
+model = whisper.load_model("large-v2")
+
+audios = os.listdir("audios")
+
+for audio in audios:
+    if "_" in audio:
+        number = audio.split("_")[0]
+        title = audio.split("_")[1][:-4]
+        print(number, title)
+
+        # 👉 Skip if JSON already exists
+        output_path = f"jsons/{audio}.json"
+        if os.path.exists(output_path):
+            print(f"Skipping {audio} (already done)")
+            continue
+
+        result = model.transcribe(
+            audio=f"audios/{audio}",
+            language="hi",
+            task="translate",
+            word_timestamps=False
+        )
+
+        chunks = []
+        for segment in result["segments"]:
+            chunks.append(
+                {
+                    "number": number,
+                    "title": title,
+                    "start": segment["start"],
+                    "end": segment["end"],
+                    "text": segment["text"]
+                }
+            )
+
+        chunks_with_metadata = {"chunks": chunks, "text": result["text"]}
+
+        # Save JSON
+        with open(output_path, "w") as f:
+            json.dump(chunks_with_metadata, f)
